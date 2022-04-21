5c0c4ce44b146d89725e01f765e239e1c547eb83
Zaur <bayernsch89@gmail.com>
2022-04-21 22:28:14 +0200

    Change variable expression

--- Great_expectations.js
+++ Great_expectations.js
@@ -6,53 +6,53 @@ function create(name) {
   return document.createElement(name);
 }
 
-let element = $(".container");
-let button = $(".theme-button");
+const element = $(".container");
+const button = $(".theme-button");
 
-button.onclick = function () {
+button.onclick =  () => {
   element.classList.toggle("dark");
 }
 
-let menuButton = $('.menu-button');
-let menuBox = $('.menu-box');
+const menuButton = $('.menu-button');
+const menuBox = $('.menu-box');
 
 menuButton.onclick = () => {
   menuBox.classList.toggle('open-menu');
 }
 
-let tooltipButton = $('.tooltip-button');
-let tooltip = $('.tooltip');
+const tooltipButton = $('.tooltip-button');
+const tooltip = $('.tooltip');
 
 tooltipButton.onclick = () => {
   tooltip.classList.toggle('tooltip-popup');
   tooltip.textContent = tooltip.dataset.tooltipText;
 }
 
-let message = $('.subscription-message')
-let email = $('.subscription-email')
-let form = $('.submission');
+const message = $('.subscription-message')
+const email = $('.subscription-email')
+const form = $('.submission');
 
 form.onsubmit = (evt) => {
   evt.preventDefault();
   message.textContent = email.value + ' has been registered on our website';
 }
 
-let article = $('article');
-let ul = create('ul');
+const article = $('article');
+const ul = create('ul');
 ul.className = "firstList";
 ul.innerHTML = "To-do list:";
 article.after(ul);
 
-let liFirst = create('li');
+const liFirst = create('li');
 liFirst.innerHTML = "learning";
 ul.append(liFirst);
 
-let liSecond = create('li');
+const liSecond = create('li');
 liSecond.innerHTML = "improving";
 ul.append(liSecond);
 
-let likesNumber = $('.likes-number');
-let heart = $('.heart');
+const likesNumber = $('.likes-number');
+const heart = $('.heart');
 
 heart.onclick = () => {
   heart.style.fill = heart.style.fill === 'red' ? null : 'red';
@@ -66,24 +66,24 @@ heart.onclick = () => {
   heart.classList.toggle('liked');
 }
 
-let commentsContainer = $('.comment-container');
-let comment = $('.comment-form');
+const commentsContainer = $('.comment-container');
+const comment = $('.comment-form');
 let counter = 1;
 
 comment.onsubmit = (evt) => {
   evt.preventDefault();
 
   // Get input element and save into a variable
-  let input = $('#comment-input');
+  const input = $('#comment-input');
 
   // Read the input's value and save into useComment variable
-  let userComment = input.value;
+  const userComment = input.value;
 
   // Create a new div element and save into newLi variable
-  let newDiv = create('div');
+  const newDiv = create('div');
 
   // Create a new span element and save into newNumberEl variable
-  let newSpan = create('span');
+  const newSpan = create('span');
 
   // Add a new class to the span element
   newSpan.classList.add('comment-number');
